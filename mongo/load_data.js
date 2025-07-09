2bad41ee332aedda1d27d346d1e9c2a7d6630427
Grace Tao <grace@abc.com>
2025-07-09 16:05:20 -0400

    blah

--- mongo/load_data.js
+++ mongo/load_data.js
@@ -1,13 +1,27 @@
-import { MongoClient, MongoNetworkError, ObjectId } from "mongodb";
+// import { MongoClient, MongoNetworkError, ObjectId } from "mongodb";
+const mongo = require('mongodb');
+const fs = require('fs');
+const path = require('path');
 
-products = []
-for(json in jsons) {
-    products.append(json)
+const folderPath = path.join(__dirname, 'json');
+
+const modules = {};
+
+fs.readdirSync(folderPath).forEach(file => {
+  if (file.endsWith('.json')) {
+    const moduleName = path.basename(file, '.json');
+    modules[moduleName] = require(path.join(folderPath, file));
+  }
+});
+
+
+let client;
+async function connect() {
+    client = await mongo.MongoClient.connect('mongodb://localhost:27017')
 }
-
-const client = await MongoClient.connect('mongodb://localhost:27017');
-
+connect();
 const db = client.db('store');
 const collection = db.collection('products');
-const socks = await collection.insertMany()
-res.json(socks);
+async function insert() {await collection.insertMany(modules)}
+
+insert();
