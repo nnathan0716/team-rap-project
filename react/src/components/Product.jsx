165a6507ddb3d59ea2681fe666f0d46bbdb89a84
Nitin Nathan <nnathan2@travelers.com>
2025-07-10 15:09:51 -0400

    added componenets and fixed some css

--- react/src/components/Product.jsx
+++ react/src/components/Product.jsx
@@ -1,8 +1,26 @@
-const Product = () => {
+import { useLocation } from "react-router-dom"
+
+const Product = (props) => {
+    const location = useLocation();
+    const productData = location.state?.info;
+    if (!productData) {
+        return <div>Loading product data...</div>;
+    }
+    
     return (
         <>
+            <h1>{productData.name}</h1>
+            <p>Price: {productData.price}</p>
+            <img 
+                src={productData.image_link} 
+                alt={`Image of ${productData.name}`}
+                onError={(e) => {
+                    e.target.onerror = null;
+                    e.target.src = "/no-image.jpg";
+                }}
+            />
         </>
-    )
+    );
 }
 
 export default Product
