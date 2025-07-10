38f5390435e4c6063d9a72ded808694ff0b26730
Grace Tao <grace@abc.com>
2025-07-10 15:01:27 -0400

    added name dialog on first load

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -3,20 +3,23 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Checkout from "./components/Checkout.jsx"
-import Navbar from "./components/Navbar.jsx"
-import Product from "./components/Product.jsx"
+import Checkout from "./components/Checkout.jsx";
+import Navbar from "./components/Navbar.jsx";
+import { StoreProvider } from "./hooks/StoreContext.jsx";
+
+import Product from "./components/Product.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path="/" element={<App />}></Route>
-        <Route path="/checkout" element={<Checkout />}></Route>
-        <Route path="/product/:name" element={<Product />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <StoreProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<App />}></Route>
+          <Route path="/checkout" element={<Checkout />}></Route>
+          <Route path="/product/:name" element={<Product />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </StoreProvider>
   </StrictMode>
 );
-
