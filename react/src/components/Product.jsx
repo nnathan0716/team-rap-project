ca1f5dc47a876add6f05aee3d7009837eb3c1eaa
Nitin Nathan <nnathan2@travelers.com>
2025-07-11 11:40:57 -0400

    product page now lets you add to ur cart

--- react/src/components/Product.jsx
+++ react/src/components/Product.jsx
@@ -1,19 +1,23 @@
 import { useLocation } from "react-router-dom"
 import "../css/Product.css"
 import "../App.css"
+import { useStoreInfo } from "../hooks/StoreContext";
 
-const Product = (props) => {
+const Product = () => {
     const location = useLocation();
     const productData = location.state?.info;
+    const { cart, setCart } = useStoreInfo();
+    
     if (!productData) {
         return <div>Loading product data...</div>;
     }
     
+    const isInCart = cart.some((product) => product._id === productData._id);
+    
     return (
         <>
         <div className="product-container">
-            <h1>{productData.name}</h1>
-                <p>Price: {productData.price}</p>
+            <div className="product-image">
                 <img 
                     src={productData.image_link} 
                     alt={`Image of ${productData.name}`}
@@ -22,8 +26,31 @@ const Product = (props) => {
                         e.target.src = "/no-image.jpg";
                     }}
                 />
-        </div>
+            </div>
+            <div className="product-info">
+                <h1>{productData.name}</h1>
+                <p>Price: {productData.price}</p>
+                <p>{productData.description}</p>    
+                {isInCart ? (
+                <button
+                onClick={() =>
+                    setCart((oldCart) =>
+                    oldCart.filter((product) => product._id !== productData._id)
+                    )
+                }
+                >
+                Remove from cart
+                </button>
+            ) : (
+                <button
+                onClick={() => setCart((oldCart) => [...oldCart, productData])}
+                >
+                Add to cart!
+                </button>
+            )}
+            </div>
             
+        </div>
         </>
     );
 }
