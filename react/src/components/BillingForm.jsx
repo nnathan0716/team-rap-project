fd099fedd09b2c5b828f373ddbc2f1d631528f77
Grace Tao <grace@abc.com>
2025-07-11 17:02:09 -0400

    added order success alert

--- react/src/components/BillingForm.jsx
+++ react/src/components/BillingForm.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import { useState } from "react";
 import "../css/BillingForm.css";
 import { useStoreInfo } from "../hooks/StoreContext";
+import { useNavigate } from "react-router-dom";
 
 const BillingForm = ({ savedItems, setDisplayBilling, purchasedItems }) => {
   const { user, orders, setOrders, setCart, total, setTotal } = useStoreInfo();
+  const navigate = useNavigate();
 
   const handleSubmitOrder = async (e) => {
     e.preventDefault();
@@ -32,72 +34,80 @@ const BillingForm = ({ savedItems, setDisplayBilling, purchasedItems }) => {
         },
         body: JSON.stringify(order),
       });
+
+      // Display alert message
+      alert("Order success!");
+
+      // Navigate to home page after alert is dismissed
+      navigate("/");
     } catch (err) {
       console.error(err);
     }
   };
 
   return (
-    <form className="form-container" onSubmit={handleSubmitOrder}>
-      <div>
-        <label>Name on Card:</label>
-        <input type="text" name="name" required />
-      </div>
-      <div>
-        <label>Credit Card Number:</label>
-        <input
-          type="text"
-          name="creditCardNumber"
-          pattern="\d{16}"
-          required
-          title="Please enter a 16-digit credit card number"
-        />
-      </div>
-      <div>
-        <label>Expiration Date:</label>
-        <input
-          type="text"
-          name="expirationDate"
-          placeholder="MM/YY"
-          pattern="\d{2}/\d{2}"
-          required
-          title="Please enter a valid expiration date in MM/YY format"
-        />
-      </div>
-      <div>
-        <label>CVV:</label>
-        <input
-          type="text"
-          name="cvv"
-          pattern="\d{3,4}"
-          required
-          title="Please enter a 3 or 4-digit CVV"
-        />
-      </div>
-      <div>
-        <label>Billing Address:</label>
-        <input type="text" name="billingAddress" required />
-      </div>
-      <div>
-        <label>City:</label>
-        <input type="text" name="city" required />
-      </div>
-      <div>
-        <label>State:</label>
-        <input type="text" name="state" required />
-      </div>
-      <div>
-        <label>Zip Code:</label>
-        <input
-          type="text"
-          name="zipCode"
-          pattern="\d{5}"
-          required
-          title="Please enter a 5-digit zip code"
-        />
-      </div>
-      <button type="submit">Place Order</button>
-    </form>
+    <div>
+      <form className="form-container" onSubmit={handleSubmitOrder}>
+        <div>
+          <label>Name on Card:</label>
+          <input type="text" name="name" required />
+        </div>
+        <div>
+          <label>Credit Card Number:</label>
+          <input
+            type="text"
+            name="creditCardNumber"
+            pattern="\d{16}"
+            required
+            title="Please enter a 16-digit credit card number"
+          />
+        </div>
+        <div>
+          <label>Expiration Date:</label>
+          <input
+            type="text"
+            name="expirationDate"
+            placeholder="MM/YY"
+            pattern="\d{2}/\d{2}"
+            required
+            title="Please enter a valid expiration date in MM/YY format"
+          />
+        </div>
+        <div>
+          <label>CVV:</label>
+          <input
+            type="text"
+            name="cvv"
+            pattern="\d{3,4}"
+            required
+            title="Please enter a 3 or 4-digit CVV"
+          />
+        </div>
+        <div>
+          <label>Billing Address:</label>
+          <input type="text" name="billingAddress" required />
+        </div>
+        <div>
+          <label>City:</label>
+          <input type="text" name="city" required />
+        </div>
+        <div>
+          <label>State:</label>
+          <input type="text" name="state" required />
+        </div>
+        <div>
+          <label>Zip Code:</label>
+          <input
+            type="text"
+            name="zipCode"
+            pattern="\d{5}"
+            required
+            title="Please enter a 5-digit zip code"
+          />
+        </div>
+        <button type="submit">Place Order</button>
+      </form>
+    </div>
   );
 };
 
